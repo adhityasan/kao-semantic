a009b01588bff07409e477a808cd47abe66222dd
Adhitya Yosua Sanjaya <adhityasanit@gmail.com>
2019-01-08 15:25:38 +0700

    add not found route on public route

--- src/routes/public.jsx
+++ src/routes/public.jsx
@@ -1,12 +1,20 @@
 import Authentication from '@views/Authentication/Authentication'
+import NotFound from '@views/Error/public_notfound'
 
 const r_Authentication = {
   path: '/',
+  exact: true,
   component: Authentication
 }
 
+const r_Notfound = {
+  path: '*',
+  component: NotFound
+}
+
 const publicRoutes = [
-  r_Authentication
+  r_Authentication,
+  r_Notfound
 ]
 
 export default publicRoutes
